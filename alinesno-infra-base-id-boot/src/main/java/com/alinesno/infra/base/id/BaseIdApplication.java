519181fdca244e58078f72cf1f0c9c2c45918869
CarrotSea233 <2733614757@qq.com>
2023-11-08 16:03:23 +0800

    AIGC

--- alinesno-infra-base-id-boot/src/main/java/com/alinesno/infra/base/id/BaseIdApplication.java
+++ alinesno-infra-base-id-boot/src/main/java/com/alinesno/infra/base/id/BaseIdApplication.java
@@ -1,5 +1,6 @@
 package com.alinesno.infra.base.id;
 
+import com.alinesno.infra.common.facade.enable.EnableActable;
 import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
 import com.sankuai.inf.leaf.plugin.annotation.EnableLeafServer;
 import org.springframework.boot.SpringApplication;
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author LuoAnDong
  * @since 2023年8月3日 上午6:23:43
  */
+@EnableActable
 @EnableInfraSsoApi
 @EnableLeafServer
 @SpringBootApplication
